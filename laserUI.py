dd550fe816829e18b8560d3509c993a321939005
Mike Erickson <merickson1982@gmail.com>
2016-03-03 15:44:28 -0800

    Added Main frame wiget

--- laserUI.py
+++ laserUI.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter.constants import *
 tk = tkinter.Tk()
-Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480)
+Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480,)
 Main.pack()
 
 
