6ae67cea7ba6a7368f7c4b0f66f79ea9e4d9ff4c
Mike Erickson <merickson1982@gmail.com>
2016-03-03 15:35:56 -0800

    Added Main frame wiget

--- laserUI.py
+++ laserUI.py
@@ -1,8 +1,10 @@
 import tkinter
 from tkinter.constants import *
 tk = tkinter.Tk()
-label = tkinter.Label(tk, text="Hello, World")
-label.pack(fill=X, expand=1)
-button = tkinter.Button(tk,text="Exit",command=tk.destroy)
-button.pack(side=BOTTOM)
+Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480)
+Main.pack()
+
+
+Exit = tkinter.Button(tk, text="Exit", command=tk.destroy)
+Exit.pack(side=BOTTOM)
 tk.mainloop()
