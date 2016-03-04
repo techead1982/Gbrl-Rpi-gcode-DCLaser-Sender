65bcfdcce6ce112aacd66677e83a4aae8e897d56
logit667 <logit667@gmail.com>
2016-03-03 21:27:36 -0800

    sender and laserUi
    Updated code to be more streamlined

--- laserUI.py
+++ laserUI.py
@@ -1,22 +1,21 @@
-import tkinter
 #import Gbrl-Rpi-gcode-DcLaser-Sender
 from tkinter import *
 from tkinter.filedialog import *   
 
 
 def openFile():
-	fn = askopenfilename(filetype=(("G code",".gcode"),("All","*.*")),title = "Get file dumb dumb")
+	fn = askopenfilename(filetype=(("G-code",".gcode"),("NC",".nc"),("All","*.*")),title = "Get file dumb dumb")
 	print(fn)
-tk = tkinter.Tk()
+root = Tk()
 
-Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480)
+Main = Frame(root, relief=RIDGE, width=800, height=480)
 Main.pack()
-#Nav = tkinter.Frame(tk, relief=SUNKEN, Main ,width=200, height=200)
+#Nav = tkinter.Frame(root, relief=SUNKEN, Main ,width=200, height=200)
 #Nav.pack()
-open = tkinter.Button(tk, text="Open",command=openFile)
+open = Button(root, text="Open",command=openFile)
 open.pack()
 open.place(height = 20,width = 40,x =60,y=10)
-Exit = tkinter.Button(tk, text="Exit", command=tk.destroy)
+Exit = Button(root, text="Exit", command=root.destroy)
 Exit.pack()
 Exit.place(height = 20,width = 40,x =10,y=10)
-tk.mainloop()
+root.mainloop()
