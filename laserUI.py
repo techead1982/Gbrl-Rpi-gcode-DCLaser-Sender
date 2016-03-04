a23c04757142e53404126711971a7a368bd42068
Mike Erickson <merickson1982@gmail.com>
2016-03-03 16:05:33 -0800

    Added Main frame wiget

--- laserUI.py
+++ laserUI.py
@@ -3,8 +3,12 @@ from tkinter.constants import *
 tk = tkinter.Tk()
 Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480)
 Main.pack()
-Nav = tkinter.Frame(tk, relief=SUNKEN, Main width=200, height=200)
-Nav.pack()
+open =tkinter.Button(tk,text="open", relief=SUNKEN)
+open.pack(side=TOP)
+
+
+#Nav = tkinter.Frame(tk, relief=SUNKEN, Main width=200, height=200)
+#Nav.pack()
 
 Exit = tkinter.Button(tk, text="Exit", command=tk.destroy)
 Exit.pack(side=BOTTOM)
