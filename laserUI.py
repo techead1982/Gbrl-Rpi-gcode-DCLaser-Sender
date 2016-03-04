7e47115eb0069893bc5389d1a60f1fa878d04678
Mike Erickson <merickson1982@gmail.com>
2016-03-03 23:04:48 -0800

    Added Main frame wiget

--- laserUI.py
+++ laserUI.py
@@ -1,21 +1,30 @@
-#import Gbrl-Rpi-gcode-DcLaser-Sender
-from tkinter import *
-from tkinter.filedialog import *   
+# import Gbrl-Rpi-gcode-DcLaser-Sender
+import tkinter
+#from tkinter import *
+from tkinter.filedialog import *
 
 
 def openFile():
-	fn = askopenfilename(filetype=(("G-code",".gcode"),("NC",".nc"),("All","*.*")),title = "Get file dumb dumb")
-	print(fn)
+    fn = askopenfilename(filetype=(("G-code", ".gcode"), ("NC", ".nc"), ("All", "*.*")), title="Get file dumb dumb")
+    print(fn)
+
+
 root = Tk()
 
-Main = Frame(root, relief=RIDGE, width=800, height=480)
-Main.pack()
-#Nav = tkinter.Frame(root, relief=SUNKEN, Main ,width=200, height=200)
-#Nav.pack()
-open = Button(root, text="Open",command=openFile)
-open.pack()
-open.place(height = 20,width = 40,x =60,y=10)
-Exit = Button(root, text="Exit", command=root.destroy)
+
+Userinfo = Frame(root, relief=RIDGE, width=400, height=120, bg="blue")
+Userinfo.place(y=0,x=100)
+
+NavC = tkinter.Frame(root, relief=SUNKEN, width=300, height=600, bg="red")
+NavC.place(y=0,x=10)
+open = Button(NavC, text="Open")
+open.pack(side="left")
+Ncsend = Button(NavC, text="send", relief=RAISED, bg="blue")
+Ncsend.pack()
+Ncstart = Button(NavC, text="Start", relief=SUNKEN, fg="black")
+Ncstart.pack()
+
+Exit = Button(NavC, fg="green", bg="black",text="Exit", command=root.destroy)
 Exit.pack()
-Exit.place(height = 20,width = 40,x =10,y=10)
+#Exit.place(height=20, width=40, x=800, y=480)
 root.mainloop()
