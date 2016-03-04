207c80bfb32d5646097eee965bf06e9555b9b003
logit667 <logit667@gmail.com>
2016-03-03 23:16:44 -0800

    calling class in gbrl sender
    note rename gbrl sender to grbl sender

--- laserUI.py
+++ laserUI.py
@@ -1,23 +1,23 @@
-# import Gbrl-Rpi-gcode-DcLaser-Sender
+import Gbrl_Rpi_gcode_DcLaser_Sender
 import tkinter
 #from tkinter import *
 from tkinter.filedialog import *
 
 
-def openFile():
-    fn = askopenfilename(filetype=(("G-code", ".gcode"), ("NC", ".nc"), ("All", "*.*")), title="Get file dumb dumb")
-    print(fn)
+def getFile():
+    print(gcodeSender.getFileInfo())
+    
 
 
 root = Tk()
-
+gcodeSender = Gbrl_Rpi_gcode_DcLaser_Sender.LaserCom()
 
 Userinfo = Frame(root, relief=RIDGE, width=400, height=120, bg="blue")
 Userinfo.place(y=0,x=100)
 
 NavC = tkinter.Frame(root, relief=SUNKEN, width=300, height=600, bg="red")
 NavC.place(y=0,x=10)
-open = Button(NavC, text="Open")
+open = Button(NavC, text="Open",command = getFile)
 open.pack(side="left")
 Ncsend = Button(NavC, text="send", relief=RAISED, bg="blue")
 Ncsend.pack()
