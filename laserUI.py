a2d339943957f2933db7244e75183d79d08607cc
Mike Erickson <merickson1982@gmail.com>
2016-03-04 00:58:35 -0800

    UI pdate

--- laserUI.py
+++ laserUI.py
@@ -8,11 +8,6 @@ def getFile():
     text1.insert(gcodeSender.getFileInfo())
     print(gcodeSender.getFileInfo())
 
-    
-#def hide():
-    #Userinfo.lower(Userinfo)
-
-
 root = Tk()
 root.geometry("800x480+100+100")
 
@@ -26,13 +21,13 @@ Send = Frame(root,width=700, bg="yellow", height=360)
 Send.place(y=120, x=100)
 
 open = Button(NavC, text="Open",command = getFile)
-open.place(y=0, x=10)
+open.place(y=100, x=0)
 Ncsend = Button(NavC, text="Send", relief=RAISED, bg="blue")
-Ncsend.place(y=10, x=20)
+Ncsend.place(y=200, x=0)
 Ncstart = Button(NavC, text="Start", relief=SUNKEN, fg="black",state="disable")
-Ncstart.place(y=20, x=30)
-Exit = Button(NavC, fg="green", bg="black",text="Exit", command=root.destroy)
-Exit.place(y=50, x=10)
+Ncstart.place(y=300, x=30)
+Exit = Button(NavC, fg="green", bg="black",text="Exit", width=80, command=root.destroy)
+Exit.place(y=460, x=0)
 text1 = Text(Send, width=700, height=300, bg="yellow")
 text1.place(y=0, x=0)
 
