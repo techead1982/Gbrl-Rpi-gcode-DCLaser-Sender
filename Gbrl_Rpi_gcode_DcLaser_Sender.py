be9e43039dbeda7d0f6cd656eeca2e3ec39b3be9
logit667 <logit667@gmail.com>
2016-03-03 22:07:09 -0800

    file rename

---
+++ Gbrl_Rpi_gcode_DcLaser_Sender.py
@@ -0,0 +1,25 @@
+import serial
+import re
+import time
+import sys
+import argparse 
+from tkinter import *
+
+class LaserCom():
+	comPort = 'COM3'
+	baudRate = 115200
+	timeOut = 1
+	fileName = ''
+	lineCount = 0
+	gcodeCount = 0
+	charline = []
+	def getFileInfo():
+		print("HI")
+	def setSerial(com,baud,tOut):
+		comPort = com
+		baudRate = baud
+		timeOut = tOut
+	def streamFile(File):
+		s = serial.Serial(comPort,baudRate,timeOut)
+		s.write("\r\n\r\n")
+		f = open(File,'r')
--- laserUI.py
+++ laserUI.py
@@ -1,18 +1,20 @@
 #import Gbrl-Rpi-gcode-DcLaser-Sender
+import Gbrl_Rpi_gcode_DcLaser_Sender
 from tkinter import *
-from tkinter.filedialog import *   
+  
 
 
-def openFile():
-	fn = askopenfilename(filetype=(("G-code",".gcode"),("NC",".nc"),("All","*.*")),title = "Get file dumb dumb")
-	print(fn)
+def getFile():
+        print("123")
 root = Tk()
+gcodeSender = Gbrl_Rpi_gcode_DcLaser_Sender.LaserCom()
 
 Main = Frame(root, relief=RIDGE, width=800, height=480)
 Main.pack()
 #Nav = tkinter.Frame(root, relief=SUNKEN, Main ,width=200, height=200)
 #Nav.pack()
-open = Button(root, text="Open",command=openFile)
+
+open = Button(root, text="Open",command = getFile)
 open.pack()
 open.place(height = 20,width = 40,x =60,y=10)
 Exit = Button(root, text="Exit", command=root.destroy)
