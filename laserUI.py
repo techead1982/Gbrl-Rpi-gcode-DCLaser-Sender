a95f2269a0627028b59caa71f63fc5beb55e8d84
logit667 <logit667@gmail.com>
2016-03-03 16:14:25 -0800

    added import of dclaser sender

--- laserUI.py
+++ laserUI.py
@@ -1,10 +1,11 @@
 import tkinter
+import Gbrl-Rpi-gcode-DcLaser-Sender
 from tkinter.constants import *
 tk = tkinter.Tk()
 Main = tkinter.Frame(tk, relief=RIDGE, width=800, height=480)
 Main.pack()
-Nav = tkinter.Frame(tk, relief=SUNKEN, Main width=200, height=200)
-Nav.pack()
+#Nav = tkinter.Frame(tk, relief=SUNKEN, Main ,width=200, height=200)
+#Nav.pack()
 
 Exit = tkinter.Button(tk, text="Exit", command=tk.destroy)
 Exit.pack(side=BOTTOM)
