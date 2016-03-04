e2d3dc637291a907081a76f2250c9c3966567d89
Mike Erickson <merickson1982@gmail.com>
2016-03-04 08:26:05 -0800

    UI pdate

--- laserUI.py
+++ laserUI.py
@@ -12,6 +12,7 @@ def getFile():
 
 root = Tk()
 root.geometry("800x480+100+100")
+root.resizable(False,False)
 
 gcodeSender = Gbrl_Rpi_gcode_DcLaser_Sender.LaserCom()
 
@@ -22,16 +23,16 @@ NavC.place(y=0,x=0)
 Send = Frame(root,width=700,  height=360, bg="yellow",)
 Send.place(y=120, x=100)
 
-open = Button(NavC, text="Open",width=10,height=10,command = getFile)
-open.place(y=50, x=0)
+Ncopen = Button(NavC, text="Open",width=10,height=10,command = getFile)
+Ncopen.place(y=5, x=0)
 Ncsend = Button(NavC, text="Send", relief=RAISED, bg="blue", height=10,)
 Ncsend.place(y=150, x=0)
 Ncstart = Button(NavC, text="Start", relief=SUNKEN, fg="black")
 Ncstart.place(y=250, x=0)
-Exit = Button(NavC, text="Exit",fg="green", bg="black", width=10, height=10, command=root.destroy)
-Exit.place(y=460, x=0)
-text1 = Text(Userinfo, width=700, height=300, bg="yellow")
-text1.place(y=0, x=0)
+NcExit = Button(NavC, text="Exit",fg="green", width=5, height=5, command=root.destroy)
+NcExit.place(y=460, x=0)
+Uitext1 = Text(Userinfo, width=70, height=30, bg="yellow")
+Uitext1.place(y=0, x=0)
 
 
 
