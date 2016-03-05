2bc6e11b8409c43cca5fbf189bb5f0e3f5d19055
Mike Erickson <merickson1982@gmail.com>
2016-03-04 19:57:29 -0800

    Update Button Width

--- laserUI.py
+++ laserUI.py
@@ -22,6 +22,7 @@ root.resizable(False,False)
 
 
 
+
 Userinfo = Frame(root, relief=RIDGE, width=700, height=120, bg="blue")
 Userinfo.place(y=0,x=100)
 NavC = Frame(root, relief=SUNKEN, width=100, height=480, bg="red")
@@ -30,14 +31,16 @@ Send = Frame(root,width=700,  height=360, bg="yellow",)
 Send.place(y=120, x=100)
 
 Ncopen = Button(NavC, text="Open",width=11,height=5,command = getFile)
-Ncopen.place(y=5, x=0)
+Ncopen.place(y=0, x=0)
 Ncsend = Button(NavC, text="Send", width=11,height=5,relief=RAISED, bg="blue")
-Ncsend.place(y=150, x=0)
+Ncsend.place(y=100, x=0)
 Ncstart = Button(NavC, text="Start",width=11,height=5, relief=SUNKEN, fg="black",command = sendFile)
-Ncstart.place(y=250, x=0)
+Ncstart.place(y=200, x=0)
 NcExit = Button(NavC, text="Exit", width=11, height=5,fg="green", command=root.destroy)
 NcExit.place(y=400, x=0)
-Uitext1 = Text(Userinfo, width=70, height=30, bg="gray")
+Ncsetting = Button(NavC, text="Settings", width=11,height=5,relief=RAISED, bg="blue")
+Ncsetting.place(y=300, x=0)
+Uitext1 = Text(Userinfo, width=70, height=30, wrap ="word",bg="gray")
 Uitext1.place(y=0, x=0)
 
 
