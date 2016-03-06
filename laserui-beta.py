7ace3e14bad945229965e8c12152e269f1b6ba0f
Mike Erickson <merickson1982@gmail.com>
2016-03-05 22:01:33 -0800

    Update Button Width

--- laserui-beta.py
+++ laserui-beta.py
@@ -13,7 +13,7 @@ f2 = ttk.Frame(notebook)
 f3 = ttk.Frame(notebook)
 f4 = ttk.Frame(notebook)
 notebook.add(f1, text='Genanl')
-notebook.add(f2, text='Setting')
+notebook.add(f2, text='File System')
 notebook.add(f3, text='Serial')
 notebook.add(f4, text='Network')
 
@@ -40,6 +40,9 @@ Gatewaylbl.grid(row=3, column=0)
 Gateway = Entry(f4,width=16)
 Gateway.grid(row=3, column=1)
 
+setnetwork=Button(f4,text="Set",width=16)
+setnetwork.grid(row=4, column=0,columnspan=2)
+
 
 portlbl = Label(f3, text ="Port", width=16)
 portlbl.grid(row=0, column=0)
@@ -58,17 +61,42 @@ databits = ttk.Combobox(f3)
 databits.config(values=('1','2','3','4','5','6','7','8','9','10'))
 databits.grid(row=3,column=1)
 
-Paritybitslbl =Label(f3, text="Paritybits")
+Paritybitslbl =Label(f3, text="Paritybits",width=16)
 Paritybitslbl.grid(row=4,column=0)
 Paritybits = ttk.Combobox(f3)
 Paritybits.config(values=('none N', 'odd (O)', 'even (E)', 'mark (M)','space (S)'))
 Paritybits.grid(row=4,column=1)
 
-stopbitslbl =Label(f3, text="Stopbits")
+stopbitslbl =Label(f3, text="Stopbits",width=16)
 stopbitslbl.grid(row=5,column=0)
 stopbits = ttk.Combobox(f3)
 stopbits.config(values=('1','2','3','4','5','6','7','8','9','10'))
 stopbits.grid(row=5,column=1)
+
+setserial=Button(f3,text="Set",width=16)
+setserial.grid(row=6, column=0,columnspan=2)
+
+homedirlbl = Label(f2, text="Home Dir",width=16)
+homedirlbl.grid(row=0,column=0)
+homedir=Entry(f2,width=16)
+homedir.grid(row=0, column=1)
+
+remotedirlbl = Label(f2, text="Remote Dir",width=16)
+remotedirlbl.grid(row=2,column=0)
+remotedir=Entry(f2,width=16)
+remotedir.grid(row=2, column=1)
+
+copytodir=Button(f2, text="Copy TO Remote Dir", width=16)
+copytodir.grid(row=3,column=0)
+
+from_dir =Button(f2, text="From TO Remote Dir", width=16)
+from_dir.grid(row=3,column=1)
+
+
+
+
+
+
 
 
 
