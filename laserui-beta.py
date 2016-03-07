493dcaa3c4009199caa9a4b27e78e193577c85da
Mike Erickson <merickson1982@gmail.com>
2016-03-06 22:16:47 -0800

    Update File System Ui

--- laserui-beta.py
+++ laserui-beta.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+from Gbrl_Rpi_gcode_DcLaser_Sender import *
+from commands import *
 
 root = Tk()
 root.geometry("800x480+100+100")
-root.resizable(False,False)
+root.resizable(False, False)
 
 notebook = ttk.Notebook(root)
 notebook.pack()
 
-f1 = ttk.Frame(notebook)   # first page, which would get widgets gridded into it
+f1 = ttk.Frame(notebook)  # first page, which would get widgets gridded into it
 f2 = ttk.Frame(notebook)
 f3 = ttk.Frame(notebook)
 f4 = ttk.Frame(notebook)
@@ -17,134 +19,111 @@ notebook.add(f2, text='File System')
 notebook.add(f3, text='Serial')
 notebook.add(f4, text='Network')
 
-static = Radiobutton(f4, text="Static")
-static.grid(row=0, column=0)
+
+def network(self):
+    self.static = Radiobutton(f4, text="Static")
+    self.static.grid(row=0, column=0)
+
 
 dhcp = Radiobutton(f4, text="Dhcp")
 dhcp.grid(row=0, column=1)
 
-ipaddress = Label(f4,text="Ip Address")
+ipaddress = Label(f4, text="Ip Address")
 ipaddress.grid(row=1, column=0)
-ipaddrv4 =Entry(f4, width=16)
+ipaddrv4 = Entry(f4, width=16)
 ipaddrv4.grid(row=1, column=1)
 
-
-
-Subnetlbl = Label(f4,text="Subnet")
+Subnetlbl = Label(f4, text="Subnet")
 Subnetlbl.grid(row=2, column=0)
-Subnet =Entry(f4, width=16)
+Subnet = Entry(f4, width=16)
 Subnet.grid(row=2, column=1)
 
-Gatewaylbl = Label(f4,text="Gateway")
+Gatewaylbl = Label(f4, text="Gateway")
 Gatewaylbl.grid(row=3, column=0)
-Gateway = Entry(f4,width=16)
+Gateway = Entry(f4, width=16)
 Gateway.grid(row=3, column=1)
 
-dns1lbl = Label(f4, text ="Dns1", width=16)
+dns1lbl = Label(f4, text="Dns1", width=16)
 dns1lbl.grid(row=4, column=0)
-dns1 =Entry(f4, width=16)
+dns1 = Entry(f4, width=16)
 dns1.grid(row=4, column=1)
 
 dns2lbl = Label(f4, text="DNS2", width=16)
 dns2lbl.grid(row=6, column=0)
-dns2 = Entry(f4,width=16)
+dns2 = Entry(f4, width=16)
 dns2.grid(row=6, column=1)
 
+setnetwork = Button(f4, text="Set", width=16)
+setnetwork.grid(row=7, column=0, columnspan=2)
 
-setnetwork=Button(f4,text="Set",width=16)
-setnetwork.grid(row=7, column=0,columnspan=2)
-
-
-portlbl = Label(f3, text ="Port", width=16)
+portlbl = Label(f3, text="Port", width=16)
 portlbl.grid(row=0, column=0)
 portcb = ttk.Combobox(f3)
-portcb.config(values=('1','2','3','4'))
+portcb.config(values=('1', '2', '3', '4'))
 portcb.grid(row=0, column=1)
 
-baudratelbl=Label(f3, text ="Baudrate", width=16)
+baudratelbl = Label(f3, text="Baudrate", width=16)
 baudratelbl.grid(row=2, column=0)
-baudrate =ttk.Combobox(f3)
-baudrate.config(values=('9600', '19200', '38400', '57600','115200'))
+baudrate = ttk.Combobox(f3)
+baudrate.config(values=('9600', '19200', '38400', '57600', '115200'))
 baudrate.grid(row=2, column=1)
 
-databitslbl =Label(f3,text="Data Bits", width=16)
+databitslbl = Label(f3, text="Data Bits", width=16)
 databitslbl.grid(row=3, column=0)
 databits = ttk.Combobox(f3)
-databits.config(values=('1','2','3','4','5','6','7','8','9','10'))
-databits.grid(row=3,column=1)
+databits.config(values=('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+databits.grid(row=3, column=1)
 
-Paritybitslbl =Label(f3, text="Paritybits",width=16)
-Paritybitslbl.grid(row=4,column=0)
+Paritybitslbl = Label(f3, text="Paritybits", width=16)
+Paritybitslbl.grid(row=4, column=0)
 Paritybits = ttk.Combobox(f3)
-Paritybits.config(values=('none N', 'odd (O)', 'even (E)', 'mark (M)','space (S)'))
-Paritybits.grid(row=4,column=1)
+Paritybits.config(values=('none N', 'odd (O)', 'even (E)', 'mark (M)', 'space (S)'))
+Paritybits.grid(row=4, column=1)
 
-stopbitslbl =Label(f3, text="Stopbits",width=16)
-stopbitslbl.grid(row=5,column=0)
+stopbitslbl = Label(f3, text="Stopbits", width=16)
+stopbitslbl.grid(row=5, column=0)
 stopbits = ttk.Combobox(f3)
-stopbits.config(values=('1','2','3','4','5','6','7','8','9','10'))
-stopbits.grid(row=5,column=1)
+stopbits.config(values=('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+stopbits.grid(row=5, column=1)
 
-setserial=Button(f3,text="Set",width=16)
-setserial.grid(row=6, column=0,columnspan=2)
+setserial = Button(f3, text="Set", width=16)
+setserial.grid(row=6, column=0, columnspan=2)
 
-homedirlbl = Label(f2, text="Home Dir",width=16)
-homedirlbl.grid(row=0,column=0)
-homedir=Entry(f2,width=16)
+homedirlbl = Label(f2, text="Home Dir", width=16)
+homedirlbl.grid(row=0, column=0)
+homedir = Entry(f2, width=16)
 homedir.grid(row=0, column=1)
 
-remotedirlbl = Label(f2, text="Remote Dir",width=16)
-remotedirlbl.grid(row=2,column=0)
-remotedir=Entry(f2,width=16)
+remotedirlbl = Label(f2, text="Remote Dir", width=16)
+remotedirlbl.grid(row=2, column=0)
+remotedir = Entry(f2, width=16)
 remotedir.grid(row=2, column=1)
 
-copytodir=Button(f2, text="Copy TO Remote Dir", width=16)
-copytodir.grid(row=3,column=0)
+copytodir = Button(f2, text="Copy TO Remote Dir", width=16)
+copytodir.grid(row=3, column=0)
 
-from_dir =Button(f2, text="From TO Remote Dir", width=16)
-from_dir.grid(row=3,column=1)
+from_dir = Button(f2, text="From TO Remote Dir", width=16)
+from_dir.grid(row=3, column=1)
 
-Usernamelbl =Label(f2,text="Username", width=16)
+Usernamelbl = Label(f2, text="Username", width=16)
 Usernamelbl.grid(row=4, column=0)
-Username =Entry(f2,width=16)
-Username.grid(row=4,column=1)
+Username = Entry(f2, width=16)
+Username.grid(row=4, column=1)
 
-Passwordlbl =Label(f2, text="Password", width=16)
+Passwordlbl = Label(f2, text="Password", width=16)
 Passwordlbl.grid(row=5, column=0)
-Password =Entry(f2, width=16)
+Password = Entry(f2, width=16)
 Password.grid(row=5, column=1)
 
-setfilesys = Button(f2, text="Set",width=16)
-setfilesys.grid(row=6, column=0,columnspan=2)
+setfilesys = Button(f2, text="Set", width=16)
+setfilesys.grid(row=6, column=0, columnspan=2)
 
-gbrllbl = Label(f1,text="Conntroler Status", width=16)
+gbrllbl = Label(f1, text="Conntroler Status", width=16)
 gbrllbl.grid(row=0, column=0)
-gbrlstatus =Label(f1,text="Connected", bg="green", fg="black",width=16)
+gbrlstatus = Label(f1, text="Connected", bg="green", fg="black", width=16)
 gbrlstatus.grid(row=0, column=2)
 
-gbrlhome = Button(f1,text="Home Laser", width=16)
+gbrlhome = Button(f1, text="Home Laser", width=16, command=hi)
 gbrlhome.grid(column=0, row=1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 root.mainloop()
