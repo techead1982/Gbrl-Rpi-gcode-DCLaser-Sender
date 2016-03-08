c9d7b1cad3acabee4cf15366f9138a891be7fa50
logit667 <logit667@gmail.com>
2016-03-08 15:19:41 -0800

    removed and recreated

--- laserui_beta_test.py
+++ laserui_beta_test.py
@@ -1,126 +1,153 @@
+import threading
 from tkinter import *
 from tkinter import ttk
-
-root = Tk()
-root.geometry("800x480+100+100")
-root.resizable(False,False)
-
-notebook = ttk.Notebook(root)
-notebook.place(width=480,height=200,x=10,y=10)
-
-f1 = ttk.Frame(notebook)   # first page, which would get widgets gridded into it
-f2 = ttk.Frame(notebook)
-f3 = ttk.Frame(notebook)
-f4 = ttk.Frame(notebook)
-notebook.add(f1, text='Genanl')
-notebook.add(f2, text='File System')
-notebook.add(f3, text='Serial')
-notebook.add(f4, text='Network')
-
-static = Radiobutton(f4, text="Static")
-static.grid(row=0, column=0)
-
-dhcp = Radiobutton(f4, text="Dhcp")
-dhcp.grid(row=0, column=1)
-
-ipaddress = Label(f4,text="Ip Address")
-ipaddress.grid(row=1, column=0)
-ipaddrv4 =Entry(f4, width=16)
-ipaddrv4.grid(row=1, column=1)
+from Gbrl_Rpi_gcode_DcLaser_Sender import *
+from commands import *
 
 
+class gSenderAPP(threading.Thread):
+	def __init__(self):
+	
+		self.root = Tk()
+		self.root.title("NobleLase v0.0.1")
+		self.root.geometry("800x480+100+100")
+		self.root.resizable(False, False)
+		self.s = StringVar()
+		
+		self.notebook = ttk.Notebook(self.root)
+		self.notebook.pack()
 
-Subnetlbl = Label(f4,text="Subnet")
-Subnetlbl.grid(row=2, column=0)
-Subnet =Entry(f4, width=16)
-Subnet.grid(row=2, column=1)
+		self.f1 = ttk.Frame(self.notebook)  # first page, which would get widgets gridded into it
+		self.f2 = ttk.Frame(self.notebook)
+		self.f3 = ttk.Frame(self.notebook)
+		self.f4 = ttk.Frame(self.notebook)
+		
+		self.notebook.add(self.f1, text='Genanl')
+		self.notebook.add(self.f2, text='File System')
+		self.notebook.add(self.f3, text='Serial')
+		self.notebook.add(self.f4, text='Network')
+		
+		self.static = Radiobutton(self.f4, text="Static")
+		self.static.grid(row=0, column=1)
+		
+		self.dhcp = Radiobutton(self.f4, text="Dhcp")
+		self.dhcp.grid(row=0, column=0)
+		
+		self.ipaddress = Label(self.f4, text="Ip Address")
+		self.ipaddress.grid(row=1, column=0)
+		self.ipaddrv4 = Entry(self.f4, width=16)
+		self.ipaddrv4.grid(row=1, column=1)
 
-Gatewaylbl = Label(f4,text="Gateway")
-Gatewaylbl.grid(row=3, column=0)
-Gateway = Entry(f4,width=16)
-Gateway.grid(row=3, column=1)
+		self.Subnetlbl = Label(self.f4, text="Subnet")
+		self.Subnetlbl.grid(row=2, column=0)
+		self.Subnet = Entry(self.f4, width=16)
+		self.Subnet.grid(row=2, column=1)
 
-setnetwork=Button(f4,text="Set",width=16)
-setnetwork.grid(row=4, column=0,columnspan=2)
+		self.Gatewaylbl = Label(self.f4, text="Gateway")
+		self.Gatewaylbl.grid(row=3, column=0)
+		self.Gateway = Entry(self.f4, width=16)
+		self.Gateway.grid(row=3, column=1)
+
+		self.dns1lbl = Label(self.f4, text="Dns1", width=16)
+		self.dns1lbl.grid(row=4, column=0)
+		self.dns1 = Entry(self.f4, width=16)
+		self.dns1.grid(row=4, column=1)
+
+		self.dns2lbl = Label(self.f4, text="DNS2", width=16)
+		self.dns2lbl.grid(row=6, column=0)
+		self.dns2 = Entry(self.f4, width=16)
+		self.dns2.grid(row=6, column=1)
+
+		self.setnetwork = Button(self.f4, text="Set", width=16)
+		self.setnetwork.grid(row=7, column=0, columnspan=2)
+
+		self.portlbl = Label(self.f3, text="Port", width=16)
+		self.portlbl.grid(row=0, column=0)
+		self.portcb = ttk.Combobox(self.f3)
+		self.portcb.config(values=('1', '2', '3', '4'))
+		self.portcb.grid(row=0, column=1)
+
+		self.baudratelbl = Label(self.f3, text="Baudrate", width=16)
+		self.baudratelbl.grid(row=2, column=0)
+		self.baudrate = ttk.Combobox(self.f3)
+		self.baudrate.config(values=('9600', '19200', '38400', '57600', '115200'))
+		self.baudrate.grid(row=2, column=1)
+
+		self.databitslbl = Label(self.f3, text="Data Bits", width=16)
+		self.databitslbl.grid(row=3, column=0)
+		self.databits = ttk.Combobox(self.f3)
+		self.databits.config(values=('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+		self.databits.grid(row=3, column=1)
+
+		self.Paritybitslbl = Label(self.f3, text="Paritybits", width=16)
+		self.Paritybitslbl.grid(row=4, column=0)
+		self.Paritybits = ttk.Combobox(self.f3)
+		self.Paritybits.config(values=('none N', 'odd (O)', 'even (E)', 'mark (M)', 'space (S)'))
+		self.Paritybits.grid(row=4, column=1)
+
+		self.stopbitslbl = Label(self.f3, text="Stopbits", width=16)
+		self.stopbitslbl.grid(row=5, column=0)
+		self.stopbits = ttk.Combobox(self.f3)
+		self.stopbits.config(values=('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+		self.stopbits.grid(row=5, column=1)
+
+		self.setserial = Button(self.f3, text="Set", width=16)
+		self.setserial.grid(row=6, column=0, columnspan=2)
+
+		self.homedirlbl = Label(self.f2, text="Home Dir", width=16)
+		self.homedirlbl.grid(row=0, column=0)
+		self.homedir = Entry(self.f2, width=16)
+		self.homedir.grid(row=0, column=1)
+
+		self.remotedirlbl = Label(self.f2, text="Remote Dir", width=16)
+		self.remotedirlbl.grid(row=2, column=0)
+		self.remotedir = Entry(self.f2, width=16)
+		self.remotedir.grid(row=2, column=1)
+
+		self.copytodir = Button(self.f2, text="Copy TO Remote Dir", width=16)
+		self.copytodir.grid(row=3, column=0)
+
+		self.from_dir = Button(self.f2, text="From TO Remote Dir", width=16)
+		self.from_dir.grid(row=3, column=1)
+
+		self.Usernamelbl = Label(self.f2, text="Username", width=16)
+		self.Usernamelbl.grid(row=4, column=0)
+		self.Username = Entry(self.f2, width=16)
+		self.Username.grid(row=4, column=1)
+
+		self.Passwordlbl = Label(self.f2, text="Password", width=16)
+		self.Passwordlbl.grid(row=5, column=0)
+		self.Password = Entry(self.f2, width=16)
+		self.Password.grid(row=5, column=1)
+
+		self.setfilesys = Button(self.f2, text="Set", width=16)
+		self.setfilesys.grid(row=6, column=0, columnspan=2)
+
+		self.gbrllbl = Label(self.f1, text="Conntroler Status", width=16)
+		self.gbrllbl.grid(row=0, column=0)
+		self.gbrlstatus = Label(self.f1, text="Connected", bg="green", fg="black", width=16)
+		self.gbrlstatus.grid(row=0, column=2)
+
+		self.gbrlhome = Button(self.f1, text="Home Laser", width=16, command=hi)
+		self.gbrlhome.grid(column=0, row=1)
+		
+		self.gbrltext = Text(self.f1,height=40,width=32,bg="gray")
+		self.gbrltext.insert('1.0',self.s.get())
+		self.gbrltext.grid(column=0, row=2,columnspan=3)
+		
+		self.root.update()
+		threading.Thread.__init__(self)
+		
+	 # def update(self):
+		 # self.root.update()
 
 
-portlbl = Label(f3, text ="Port", width=16)
-portlbl.grid(row=0, column=0)
-portcb = ttk.Combobox(f3)
-portcb.config(values=('1','2','3','4'))
-portcb.grid(row=0, column=1)
-
-baudratelbl=Label(f3, text ="Baudrate", width=16)
-baudratelbl.grid(row=2, column=0)
-baudrate =ttk.Combobox(f3)
-baudrate.config(values=('9600', '19200', '38400', '57600','115200'))
-baudrate.grid(row=2, column=1)
-
-databitslbl =Label(f3,text="Data Bits", width=16)
-databitslbl.grid(row=3, column=0)
-databits = ttk.Combobox(f3)
-databits.config(values=('1','2','3','4','5','6','7','8','9','10'))
-databits.grid(row=3,column=1)
-
-Paritybitslbl =Label(f3, text="Paritybits",width=16)
-Paritybitslbl.grid(row=4,column=0)
-Paritybits = ttk.Combobox(f3)
-Paritybits.config(values=('none N', 'odd (O)', 'even (E)', 'mark (M)','space (S)'))
-Paritybits.grid(row=4,column=1)
-
-stopbitslbl =Label(f3, text="Stopbits",width=16)
-stopbitslbl.grid(row=5,column=0)
-stopbits = ttk.Combobox(f3)
-stopbits.config(values=('1','2','3','4','5','6','7','8','9','10'))
-stopbits.grid(row=5,column=1)
-
-setserial=Button(f3,text="Set",width=16)
-setserial.grid(row=6, column=0,columnspan=2)
-
-homedirlbl = Label(f2, text="Home Dir",width=16)
-homedirlbl.grid(row=0,column=0)
-homedir=Entry(f2,width=16)
-homedir.grid(row=0, column=1)
-
-remotedirlbl = Label(f2, text="Remote Dir",width=16)
-remotedirlbl.grid(row=2,column=0)
-remotedir=Entry(f2,width=16)
-remotedir.grid(row=2, column=1)
-
-copytodir=Button(f2, text="Copy TO Remote Dir", width=16)
-copytodir.grid(row=3,column=0)
-
-from_dir =Button(f2, text="From TO Remote Dir", width=16)
-from_dir.grid(row=3,column=1)
-
-Usernamelbl =Label(f2,text="Username", width=16)
-Usernamelbl.grid(row=4, column=0)
-Username =Entry(f2,width=16)
-Username.grid(row=4,column=1)
-
-Passwordlbl =Label(f2, text="Password", width=16)
-Passwordlbl.grid(row=5, column=0)
-Password =Entry(f2, width=16)
-Password.grid(row=5, column=1)
-
-setfilesys = Button(f2, text="Set",width=16)
-setfilesys.grid(row=6, column=0,columnspan=2)
 
 
+		 
+if __name__=='__main__':
+	app = gSenderAPP()
+	app.start()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-root.mainloop()
+# root.mainloop()
