70b98ca6929f7181a4fd9acc82a30c28f62a4fda
Mike Erickson <merickson1982@gmail.com>
2016-03-16 15:50:13 -0700

    drop old fiel and

--- tree stuff.py
+++ tree stuff.py
@@ -5,8 +5,11 @@ from tkinter import ttk
 tk = Tk()
 
 
-tree = ttk.Treeview()
-tree.insert('', 'end', 'widgets', text='Widget Tour')
+tree = ttk.Treeview(columns=('col0','col2','col3'))
+tree.heading('#0', text='col1')
+tree.heading('col2', text='col2')
+tree.heading('col3', text='col3')
+# tree.column('col0',stretch=NO,minwidth=0,width=0)
 
 tree.pack()
 tk.mainloop()
