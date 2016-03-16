1a2a7d54416d4e03c069e46b4bf536e605edf9bf
Mike Erickson <merickson1982@gmail.com>
2016-03-16 15:53:14 -0700

    drop old fiel and

--- tree stuff.py
+++ tree stuff.py
@@ -1,10 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 
-
 tk = Tk()
-
-
 tree = ttk.Treeview(columns=('col0','col2','col3'))
 tree.heading('#0', text='col1')
 tree.heading('col2', text='col2')
