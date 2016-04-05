0f220f571392828af7aafb275a332495dbbf5661
Mike Erickson <merickson1982@gmail.com>
2016-04-04 17:05:20 -0700

    Update File System Ui

--- Working/Test Serial Mon.py
+++ Working/Test Serial Mon.py
@@ -1,11 +1,12 @@
-from serial import *
+import serial
 from tkinter import *
 
-serialPort = "/dev/ttyACM0"
-baudRate = 9600
-ser = Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
+serialPort = "/dev/cu.usbmodem1D11"
+baudRate = 115200
+ser = serial.Serial(serialPort , baudRate, timeout=0, writeTimeout=0) #ensure non-blocking
 
 #make a TkInter Window
+
 root = Tk()
 root.wm_title("Reading Serial")
 
@@ -14,7 +15,7 @@ scrollbar = Scrollbar(root)
 scrollbar.pack(side=RIGHT, fill=Y)
 
 # make a text box to put the serial output
-log = Text ( root, width=30, height=30, takefocus=0)
+log = Text ( root, width=20, height=10, takefocus=0)
 log.pack()
 
 # attach text box to scrollbar
@@ -24,19 +25,49 @@ scrollbar.config(command=log.yview)
 #make our own buffer
 #useful for parsing commands
 #Serial.readline seems unreliable at times too
-serBuffer = ""
+
+
+v=StringVar()
+v.set("hi")
+
+e = Entry(root, textvariable=v)
+e.pack()
+
+def sendBut():
+    global eatsh
+    s = v.get()
+    i = 0
+    print(s)
+    eatsh = s+"\n"
+
+
+b = Button(root, text="Send", command=sendBut)
+b.pack()
+
+
+
+
 
 def readSerial():
+    global serBuffer
+    global eatsh
+
+
     while True:
-        c = ser.read() # attempt to read a character from Serial
+        c = ser.read().decode('ascii') # attempt to read a character from Serial
 
         #was anything read?
         if len(c) == 0:
             break
 
         # get the buffer from outside of this function
-        global serBuffer
+        # serBuffer
+        # eatsh
 
+        if eatsh !="":
+
+            ser.write(eatsh.encode('ascii'))
+            eatsh =""
         # check if character is a delimeter
         if c == '\r':
             c = '' # don't want returns. chuck it
@@ -52,6 +83,11 @@ def readSerial():
 
     root.after(10, readSerial) # check serial again soon
 
+
+
+
+serBuffer =""
+eatsh = ""
 
 # after initializing serial, an arduino may need a bit of time to reset
 root.after(100, readSerial)
